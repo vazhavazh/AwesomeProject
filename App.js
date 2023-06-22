aebaac59c980c4b8d1822544251268032dd53cf6
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-06-22 13:55:25 +0300

    registerScreen created

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ import { useCallback } from 'react';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import RegistrationScreen from './screens/RegistrationScreen/RegistrationScreen'
 import BgPicture from './assets/img/bg.png'
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto 400': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto 500': require('./assets/fonts/Roboto-Medium.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
 
     <View style={styles.container}>
@@ -23,36 +33,10 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
 });
 
 
 
 
-
-// import React from 'react';
-// import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-
-// const image = { uri: 'https://reactjs.org/logo-og.png' };
-
-// const App = () => (
-//   <View style={styles.container}>
-//     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-
-//     </ImageBackground>
-//   </View>
-// );
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   image: {
-//     flex: 1,
-//     justifyContent: 'center',
-//   },
-
-// });
-
-// export default App;
--- screens/RegistrationScreen/RegistrationScreen.js
+++ screens/RegistrationScreen/RegistrationScreen.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, TextInput, Button } from 'react-native';
 
 const RegistrationScreen = () => {
-    const handlePress = () => {
-        console.log('Button pressed!');
+    const handleEditPhoto = () => {
+        console.log('edit Button pressed!');
     };
 
+    const handleRegistration = () => {
+        console.log('register button pressed')
+    }
+
+    const handleAlreadyRegisterLink = () => {
+        console.log('link pressed')
+    }
+
+    
     return (
         <View style={styles.registerContainer}>
             <View style={styles.avatarContainer}>
-                {/* <Image /> */}
                 <TouchableOpacity
-                    style={styles.avatarButton}
-                    onPress={handlePress}
+                    style={styles.editAvatarButton}
+                    onPress={handleEditPhoto}
                 >
                     <View style={styles.horizontalLine} />
                     <View style={styles.verticalLine} />
                 </TouchableOpacity>
             </View>
+            <Text style={styles.registerTitle}>Реєстрація</Text>
+            <View style={styles.registerInputContainer}>
+                <TextInput
+                    style={styles.registerInput}
+                    placeholder='Логін'
+
+                />
+                <TextInput style={styles.registerInput} placeholder='Адреса електронної пошти' />
+                <TextInput style={styles.registerInput} placeholder='Пароль' />
+            </View>
+            <TouchableOpacity
+                style={styles.registerButton}
+                onPress={handleRegistration}
+            >
+                <Text style={styles.registerButtonText}>Зареєстуватися</Text>
+            </TouchableOpacity>
+            <Text onPress={handleAlreadyRegisterLink} style={styles.alreadyRegisterLink}>Вже є акаунт? Увійти</Text>
         </View>
     );
 };
@@ -46,7 +71,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#F6F6F6',
         borderRadius: 16,
     },
-    avatarButton: {
+    editAvatarButton: {
         position: 'absolute',
         width: 25,
         height: 25,
@@ -71,4 +96,56 @@ const styles = StyleSheet.create({
         height: 13,
         backgroundColor: '#FF6C00',
     },
+    registerTitle: {
+        marginBottom: 33,
+
+        color: '#212121',
+        textAlign: 'center',
+        textShadowColor: 'rgba(0, 0, 0, 0.25)',
+        textShadowOffset: { width: 0, height: 4 },
+        textShadowRadius: 4,
+        fontSize: 30,
+        fontFamily: 'Roboto 500',
+        letterSpacing: 0.3,
+    },
+    registerInputContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 16,
+        marginBottom: 43,
+    },
+    registerInput: {
+        fontSize: 16,
+        fontFamily: 'Roboto 400',
+        backgroundColor: '#F6F6F6',
+        borderRadius: 16,
+        borderWidth: 1,
+        borderColor: '#E8E8E8',
+        paddingLeft: 16,
+        paddingTop: 16,
+        paddingBottom: 16,
+    },
+    registerButton: {
+        paddingTop: 16,
+        paddingRight: 32,
+        paddingBottom: 16,
+        paddingLeft: 32,
+        fontSize: 16,
+        fontFamily: 'Roboto 400',
+        borderRadius: 100,
+        backgroundColor: '#FF6C00',
+        alignItems: 'center',
+    },
+    registerButtonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+    alreadyRegisterLink: {
+        marginTop: 16,
+
+        color: '#1B4371',
+        textAlign: 'center',
+        fontSize: 16,
+        fontFamily: 'Roboto 400',
+    }
 });
